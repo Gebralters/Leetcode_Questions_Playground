e84e714010360eb0f4e67a43929a3e04dfe1bf16
Letheba Kaka <lethebagebralters@gmail.com>
2025-07-13 20:08:52 +0200

    Updates to my Playground

---
+++ algorithmplayground/src/main/java/com/lg202507/string/DisplayDiamond.java
@@ -0,0 +1,25 @@
+package com.lg202507.string;
+
+public class DisplayDiamond {
+    public static void main(String[] args) {
+        int xCenter=11;
+        int yCenter=7;
+        int size=4;
+
+        int length=14;
+        int width=22;
+
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
